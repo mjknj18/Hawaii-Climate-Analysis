7459f97eced185b88a1a36d0366bc38fe02eb349
Matt Kennedy <mjknj18@gmail.com>
2019-11-13 20:27:41 -0500

    Updated Flask App Code.

--- Hawaii_Climate_Analysis_App.py
+++ Hawaii_Climate_Analysis_App.py
@@ -32,7 +32,7 @@ def home():
         <h3>Stations (/api/v1.0/stations)</h3>
         <p>Returns a list of all Hawaii weather stations.</p>
         <h3>Temperature (/api/v1.0/tobs)</h3>
-        <p>Returns Hawaii temperature data for the most recent year on record (2016-08-24 through 2017-08-23).</p>'''
+        <p>Returns Hawaii temperature data for the most active weather station for recent year on record (2016-08-24 through 2017-08-23).</p>'''
 
 @app.route('/api/v1.0/precipitation', methods=['GET'])
 def api_precip():
@@ -65,8 +65,54 @@ def api_precip():
     precip_data = precip_data.sort_index()
 
     #Convert Data Frame to Dictionary
-    precip_dict = precip_data.to_dict()
+    precip_dict = {date_name: date_group['prcp'].tolist() for date_name, date_group in precip_data.groupby('date')}
 
     return jsonify(precip_dict)
+
+@app.route('/api/v1.0/stations', methods=['GET'])
+def api_stations():
+    active_stations = engine.execute('SELECT station, name FROM Station')
+
+    station_list = {}
+
+    for occurences in active_stations:
+        station_list.update({occurences[0]: occurences[1]})
+
+    return jsonify(station_list)
+
+@app.route('/api/v1.0/tobs', methods=['GET'])
+def api_temp():
+    #Extract All Avialable Dates from Measurements Table
+    date_list = engine.execute('SELECT date FROM Measurement')
+
+    #Loop Thruough Dates to Find Latest Record
+    for date in date_list:
+        last_date = date[0]
+    
+    #Split Lastest Date Record at Dashes to Isolate Year/Month/Day
+    date_split = last_date.split('-')
+
+    #Subtract One from Year Index
+    date_split[0] = str(int(date_split[0]) - 1)
+
+    #Re-Assemble Date Value & Record as Year Prior Date
+    prior_date = date_split[0] + '-' + date_split[1] + '-' + date_split[2]
+
+    #Query Database to Create Pandas Data Frame of Precipitation Data for Selected Year-Long Period for Most Active Weather Station
+    temp_data = pd.read_sql('SELECT date, tobs FROM Measurement WHERE date >= (?) AND station = (?)', engine, params = (prior_date, 'USC00519281',))
+
+    #Drop Rows with Missing Values
+    temp_data = temp_data.dropna()
+
+    #Set Date Values as Data Frame Index
+    temp_data = temp_data.set_index('date')
+
+    #Sort Data Frame by Date
+    temp_data = temp_data.sort_index()
+
+    #Convert Data Frame to Dictionary
+    temp_dict = {date_name: date_group['tobs'].tolist() for date_name, date_group in temp_data.groupby('date')}
+
+    return jsonify(temp_dict)
 
 app.run()
